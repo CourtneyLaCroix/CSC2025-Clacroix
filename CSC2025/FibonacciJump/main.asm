c69de790834fbf86968e8460777241045243aee8
Courtney LaCroix <courtney.lacroix@outlook.com>
2025-03-31 15:25:02 -0600

    Kishore Check-in

--- CSC2025/FibonacciJump/main.asm
+++ CSC2025/FibonacciJump/main.asm
@@ -15,23 +15,22 @@ _main:
 	mov		edx, 1		;fib(n-1)							current
 	mov		ebx, 1		;fib(n-2) until moved into eax		prev
 
-	mov		ecx, 5		;num to cmp to 
-
+	mov		edi, 5		;num to cmp to 
+	mov		ecx, 0
 	_fib:
-	cmp		eax, ecx
-	;jae		_exit:   need to get this flag to work
+	cmp		edi, ecx
+	jae		_exit   ;need to get this flag to work
 
 
 	add		edx, ebx			;next = current + previous
-	add		eax, edx
+	mov		eax, edx
 	mov		ebx, edx			;prev = current
 	mov		edx, eax			;current = next;
-
-
+	inc		ecx
+	
 	jmp		_fib
 
-
-	_exit:
+_exit:
 
 
 	push	0
